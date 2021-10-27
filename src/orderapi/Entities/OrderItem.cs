8bc362f68f01f583fbbeb11e4711307acf784554
Chaiminda Chandrasekara <chaminda@ekriegers.com>
2021-10-27 13:59:59 +0530

    Intial Migrations added

--- src/orderapi/Entities/OrderItem.cs
+++ src/orderapi/Entities/OrderItem.cs
@@ -14,6 +14,7 @@ namespace orderapi.Entities
         public int Id { get; set; }
         public string ItemName { get; set; }
         public int Quantity { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
         public decimal Price { get; set; }
 
         [ForeignKey("Order")]
